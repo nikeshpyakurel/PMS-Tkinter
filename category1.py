546ac2cacb4b5a0a001354776a3869463120acef
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-07-21 00:18:32 +0545

    all changes

--- category1.py
+++ category1.py
@@ -11,7 +11,7 @@ height_screen = root.winfo_screenheight()
 root.title("Medicine/pharmacy_management_system")
 root.geometry(f"{width_screen}x{height_screen}")
 root.minsize("400", "300")
-root.iconbitmap('medicine.ico')
+root.iconbitmap('./assets/medicine.ico')
 root.state("zoomed")
 # ****************************dashboard frame************************************
 frame = Frame(width=160, padx=30, bg='#363740')
@@ -19,7 +19,7 @@ frame.pack(side=LEFT, fill=Y)
 
 
 # ****************************logo image************************************
-logoImg = PhotoImage(file='logo.png')
+logoImg = PhotoImage(file='./assets/logo.png')
 logo = Label(image=logoImg, bg='white', fg='black', width=159, height=27)
 logo.place(x=0, y=0)
 
@@ -56,12 +56,12 @@ def category():
 def billing():
     root.destroy()
     import billing
-# settings function
+# setting function
 
 
-def settings():
+def setting():
     root.destroy()
-    import settings
+    import setting
 
 # add function
 
@@ -77,7 +77,7 @@ def update():
     top = Toplevel(root)
     top.title("update category credentials")
     top.geometry(f"{width_screen}x{height_screen}")
-    top.iconbitmap('medicine.ico')
+    top.iconbitmap('./assets/medicine.ico')
     top.state('zoomed')
 
     def edit():
@@ -86,7 +86,7 @@ def update():
         else:
             try:
                 con = mysql.connect(host='localhost', user='root',
-                                    password="saroj@0777", port="3306", database='pharmacy')
+                                    password="", port="3306", database='pharmacy_management_system')
                 mycursor = con.cursor()
             except:
                 messagebox.showerror("error", "connection error")
@@ -158,7 +158,7 @@ medicine.place(x=10, y=90)
 category = Button(text='Category', width=19, height=2, border=0, bg='#595B67',
                   fg='white', font=("Inter", 10, "bold"), cursor='hand2', command=category)
 category.place(x=1, y=130)
-categoryimg = PhotoImage(file='category.png')
+categoryimg = PhotoImage(file='./assets/category.png')
 img = Label(root, image=categoryimg, bg='#595B67')
 img.place(x=6, y=135)
 # indicator for category
@@ -169,7 +169,7 @@ billing = Button(text='Billing', border=0, bg='#363740', fg='white', font=(
     "Inter", 10, "bold"), cursor='hand2', command=billing)
 billing.place(x=10, y=170)
 setting = Button(text='Pharmacy Setting', border=0, bg='#363740', fg='white', font=(
-    "Inter", 10, "bold"), cursor='hand2', command=settings)
+    "Inter", 10, "bold"), cursor='hand2', command=setting)
 setting.place(x=10, y=210)
 
 # ***************log out button*******************************
@@ -188,12 +188,12 @@ heading = Label(frame1, text='ABC Pharmacy Store', bg='#FAFAFA',
 heading.place(x=100, y=5)
 
 # ************* profile image****************************
-profileImg = PhotoImage(file='profile.png')
+profileImg = PhotoImage(file='./assets/profile.png')
 profile = Label(image=profileImg, fg='black', bg='#FAFAFA')
 profile.place(x=1050, y=0)
 name = Label(text="Admin", fg='black', bg='#FAFAFA', font=("Poppins", 10))
 name.place(x=1100, y=25)
-name = Label(text="Saroj Kumar Sah", bg='#FAFAFA',
+name = Label(text="Test", bg='#FAFAFA',
              fg='black', font=("Poppins", 10))
 name.place(x=1100, y=5)
 
@@ -231,7 +231,7 @@ categoryarea.pack(fill=BOTH, expand=1)
 # data base
 try:
     con = mysql.connect(host='localhost', user='root',
-                        password="saroj@0777", port="3306", database='pharmacy')
+                        password="", port="3306", database='pharmacy_management_system')
     mycursor = con.cursor()
 except:
     messagebox.showerror("error", "connection error")
