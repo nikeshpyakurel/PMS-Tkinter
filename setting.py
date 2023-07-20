cb53c2204b322ec62c5a3ecf8845e24aed582014
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-07-21 00:27:43 +0545

    setting completed

--- setting.py
+++ setting.py
@@ -9,7 +9,7 @@ height_screen = root.winfo_screenheight()
 root.title("Medicine/pharmacy_management_system")
 root.geometry(f"{width_screen}x{height_screen}")
 root.minsize("400", "300")
-root.iconbitmap('medicine.ico')
+root.iconbitmap('./assets/medicine.ico')
 root.state("zoomed")
 # ****************************dashboard frame************************************
 frame = Frame(width=160, padx=30, bg='#363740')
@@ -17,7 +17,7 @@ frame.pack(side=LEFT, fill=Y)
 
 
 # ****************************logo image************************************
-logoImg = PhotoImage(file='logo.png')
+logoImg = PhotoImage(file='./assets/logo.png')
 logo = Label(image=logoImg, bg='white', fg='black', width=159, height=27)
 logo.place(x=0, y=0)
 
@@ -54,12 +54,12 @@ def category():
 def billing():
     root.destroy()
     import billing
-# settings function
+# setting function
 
 
-def settings():
+def setting():
     root.destroy()
-    import settings
+    import setting
 
 # username  function
 
@@ -77,25 +77,25 @@ def on_leave(e):
 
 
 def show():
-    eyeimg.config(file='icons8-eye-24.png')
+    eyeimg.config(file='./assets/icons8-eye-24.png')
     new_password.config(show="")
     eyebutton.config(command=hide)
 
 
 def hide():
-    eyeimg.config(file='icons8-closed-eye-24.png')
+    eyeimg.config(file='./assets/icons8-closed-eye-24.png')
     new_password.config(show='*')
     eyebutton.config(command=show)
 
 
 def show1():
-    eyeimg2.config(file='icons8-eye-24.png')
+    eyeimg2.config(file='./assets/icons8-eye-24.png')
     confirm_password.config(show="")
     eyebutton2.config(command=hide1)
 
 
 def hide1():
-    eyeimg2.config(file='icons8-closed-eye-24.png')
+    eyeimg2.config(file='./assets/icons8-closed-eye-24.png')
     confirm_password.config(show='*')
     eyebutton2.config(command=show1)
 
@@ -103,7 +103,7 @@ def hide1():
 def save():
     try:
         con = mysql.connect(host='localhost', user='root',
-                            password="saroj@0777", port="3306", database='pharmacy')
+                            password="", port="3306", database='pharmacy_management_system')
         mycursor = con.cursor()
     except:
         messagebox.showerror("error", "connection error")
@@ -141,7 +141,7 @@ billing = Button(text='Billing', border=0, bg='#363740', fg='white', font=(
     "Inter", 10, "bold"), cursor='hand2', command=billing)
 billing.place(x=10, y=170)
 setting = Button(text='Pharmacy Setting', width=19, border=0, bg='#595B67',
-                 fg='white', font=("Inter", 10, "bold"), cursor='hand2', command=settings)
+                 fg='white', font=("Inter", 10, "bold"), cursor='hand2', command=setting)
 setting.place(x=1, y=210, height=40)
 # indicator for setting
 setting_indicator = Button(root, text='', bg='#FF702A')
@@ -164,21 +164,21 @@ heading = Label(frame1, text='ABC Pharmacy Store', bg='#FAFAFA',
 heading.place(x=100, y=5)
 
 # ************* profile image****************************
-profileImg = PhotoImage(file='profile.png')
+profileImg = PhotoImage(file='./assets/profile.png')
 profile = Label(image=profileImg, fg='black', bg='#FAFAFA')
 profile.place(x=1050, y=0)
 name = Label(text="Admin", fg='black', bg='#FAFAFA', font=("Poppins", 10))
 name.place(x=1100, y=25)
-name = Label(text="Saroj Kumar Sah", bg='#FAFAFA',
+name = Label(text="Test", bg='#FAFAFA',
              fg='black', font=("Poppins", 10))
 name.place(x=1100, y=5)
 
 # frame for displaying setting details
 display_frame = Frame(root, bg='#FAFAFA', width=624, height=550)
 display_frame.place(x=350, y=100)
-settings = Label(root, text='Pharmacy Settings', fg='#363740',
-                 bg='#fafafa', font=('inter', 15, 'bold'))
-settings.place(x=360, y=110)
+setting = Label(root, text='Pharmacy setting', fg='#363740',
+                bg='#fafafa', font=('inter', 15, 'bold'))
+setting.place(x=360, y=110)
 # username
 username = Entry(root, fg='black', bg='#FAFAFA', border=1, font=(
     'microsoft yaHei UI light', 15), width=40, cursor='hand2')
@@ -207,7 +207,7 @@ new_password.place(x=440, y=300, height=40)
 new_password.insert(5, 'New Password')
 new_password.bind('<FocusIn>', on_enter)
 new_password.bind('<FocusOut>', on_leave)
-eyeimg = PhotoImage(file='icons8-closed-eye-24.png')
+eyeimg = PhotoImage(file='./assets/icons8-closed-eye-24.png')
 eyebutton = Button(root, image=eyeimg, bd=0, bg='white',
                    activebackground='white', cursor='hand2', command=show)
 eyebutton.place(x=855, y=310)
@@ -232,7 +232,7 @@ confirm_password.place(x=440, y=400, height=40)
 confirm_password.insert(5, 'Confirm Password')
 confirm_password.bind('<FocusIn>', on_enter)
 confirm_password.bind('<FocusOut>', on_leave)
-eyeimg2 = PhotoImage(file='icons8-closed-eye-24.png')
+eyeimg2 = PhotoImage(file='./assets/icons8-closed-eye-24.png')
 eyebutton2 = Button(root, image=eyeimg2, bd=0, bg='white',
                     activebackground='white', cursor='hand2', command=show1)
 eyebutton2.place(x=855, y=410)
